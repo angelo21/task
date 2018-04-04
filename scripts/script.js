5f96baaf4874335c9b562d85050b6318e80fd0c8
Tom LanFranca <ittechtommy@gmail.com>
2018-04-04 15:58:28 -0500

    Made form functional with JS. Can now add items to list

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,44 @@
 
+//DEFINE VARIABLES
+const form = document.querySelector(".js-card__form");
+const taskList = document.querySelector(".js-card__collection");
+const clearBtn = document.querySelector(".js-card__clear-tasks");
+const filter = document.querySelector(".js-filter");
+const taskInput = document.querySelector(".js-task")
+
+// LOAD ALL EVENT LISTENERS
+function loadEventListeners() {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (taskInput.value === "") {
+      alert("Add a task");
+    }
+
+    //CREATE LI ELEMENT
+    const li =  document.createElement("li");
+                li.className ="card__collection-item";
+                li.appendChild(document.createTextNode(taskInput.value));
+
+    //CREATE LINK ELEMENT
+    const a = document.createElement("a");
+          a.setAttribute("href", "#");
+          a.className = "card__delete";
+          a.innerHTML = "<i class='card__delete-item'><span class='card__delete-button'>X</span></i>";
+          li.appendChild(a);
+
+    //APPEND LI TO UL
+    taskList.appendChild(li);
+
+    //CLEAR THE INPUT
+    taskInput.value = "";
+  });
+}
+
+loadEventListeners();
+
+
+
+
+
+
+
